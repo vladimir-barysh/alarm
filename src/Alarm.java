46dbe5de833cf68bf5820fe54dfa89c25d6cbebe
Vladimir <megabariga2015@gmail.com>
2026-03-01 17:24:59 +0700

    Refactoring multiply() trivial implementation

--- src/Alarm.java
+++ src/Alarm.java
@@ -9,7 +9,7 @@ public class Alarm {
     public String ring(String time) {
         //TODO: реализовать метод до конца позднее
         if (time.equals("06:00") || time.equals("06:01")) return "RING!";
-        throw new IllegalArgumentException("ring() works only with 06:00");
+        else return "Silence...";
     }
 
     /**
