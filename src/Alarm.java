2d3569614edf4c1f6827d7d45f71c75b7fd9818b
Vladimir <megabariga2015@gmail.com>
2026-03-01 16:43:51 +0700

    Javadoc for ring()

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,6 +1,11 @@
 public class Alarm {
     public Alarm() {}
 
+    /**
+     * Метод, который звонит в определенное время
+     * @param time время, в которое нужно звонить. Строка
+     * @return результат проверки времени
+     */
     public String ring(String time) {
         //TODO: реализовать метод до конца позднее
         if (time.equals("06:00") || time.equals("06:01")) return "RING!";
