fddba22dd4b65fc91d520c5fd90a96524075f506
Vladimir <megabariga2015@gmail.com>
2026-03-01 16:25:11 +0700

    Added test for Alarm.ring(6:00)

--- test/AlarmTest.java
+++ test/AlarmTest.java
@@ -1,10 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AlarmTest {
+    private Alarm alarm;
+
+    @BeforeEach
+    void setUp() {
+        alarm = new Alarm();
+    }
+
     @Test
     void alarmClassCreationtest() {
-        Alarm alarm = new Alarm();
         assertNotNull(alarm);
+    }
+
+    @Test
+    void alarmRingTest() {
+        assertEquals("RING!", alarm.ring("06:00"));
     }
 }
