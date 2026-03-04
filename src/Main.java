f036c6297621e3e7f965df3dd95e1b26c192a79a
Vladimir <megabariga2015@gmail.com>
2026-03-04 16:34:20 +0700

    changed Main to work with delayed alarm

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,43 @@
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Alarm alarm = new Alarm();
+
+        System.out.println("Введите день недели когда сработает будильник, пустая строка – выход:");
+        String day = sc.nextLine();
+
+        System.out.println("Установите время для будильника:");
+        String time = sc.nextLine();
+        System.out.println("Будильник установлен на " + time + ". Ожидание...");
+
         while (true) {
-            System.out.println("Введите день недели, когда будет срабатывать будильник, пустая строка – выход");
-            String day = sc.nextLine();
-            if (day.isEmpty()) break;
             if (alarm.checkDayIsToday(day)) {
-                System.out.println("Установите время для будильника:");
-                String time = sc.nextLine();
-                System.out.println("Результат: " + alarm.ring(time));
+                boolean hasRung = false;
+                while (!hasRung) {
+                    String status = alarm.ring(time);
+
+                    if (status.equals("RING!")) {
+                        System.out.println("*  " + status + "  *");
+                        hasRung = true;
+                    } else {
+                        try {
+                            Thread.sleep(5000); //проверка каждые 5 сек
+                        } catch (InterruptedException e) {
+                            System.out.println("Ошибка ожидания.");
+                        }
+                    }
+                }
+                break;
             } else {
                 System.out.println("Этот день не сегодня. Будильник не сработает.");
+                try {
+                    Thread.sleep(60000); //проверка каждую минуту
+                } catch (InterruptedException e) {
+                    System.out.println("Ошибка ожидания.");
+                }
             }
         }
     }
